329c496abc6f036d8ff4398c2d89c178f24b57a0
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-10-31 18:31:47 +0530

    net: Import net changes from RMX1911

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1647,6 +1647,14 @@ int tcp_v4_rcv(struct sk_buff *skb)
 	if (!pskb_may_pull(skb, th->doff * 4))
 		goto discard_it;
 
+    #ifdef ODM_WT_EDIT
+    //huan.Yang@ODM_WT.NW.DATA.1372106, 2018/04/22, Add for if the csum invalid, drop the packet.
+    /* Assuming a trustworthy entity did the checksum and found the csum
+    invalid, drop the packet.*/
+    if (skb->ip_summed == CHECKSUM_COMPLETE && skb->csum_valid == 0)
+          goto csum_error;
+    #endif /* ODM_WT_EDIT */
+
 	/* An explanation is required here, I think.
 	 * Packet length and doff are validated by header prediction,
 	 * provided case of th->doff==0 is eliminated.
